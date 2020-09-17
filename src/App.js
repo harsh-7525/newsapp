1ab50fad461cbc9d55b48d229d23835feb785128
Harsh Kant <harshkant87@gmail.com>
2020-09-17 18:25:08 +0530

    fourth commit integrated news api and views

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { useEffect } from "react"
-import alanBtn from "@alan-ai/alan-sdk-web"
+import React, { useState,useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey = "a2589834fc4ac7bf292ee75776169ed62e956eca572e1d8b807a3e2338fdd0dc/stage"
 const App = () => {
+
+  const [newsArticles, setNewsArticles] = useState([]);
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
         if (command === "newHeadlines") {
-          console.log(articles)
+          setNewsArticles(articles);
         }
       }
     })
@@ -17,6 +22,7 @@ const App = () => {
   return (
     <div>
       <h1>Alan AI News Applications</h1>
+      <NewsCards articles = {newsArticles} />
     </div>
   )
 }
