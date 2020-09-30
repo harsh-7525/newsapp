f5366445f26fddac7b09052f54e02e7fdf04c84a
Harsh Kant <harshkant87@gmail.com>
2020-09-30 21:36:16 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React,{useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
@@ -14,7 +16,7 @@ const App=()=>{
     useEffect(()=> { 
         alanBtn({
         key: alankey,  
-        onCommand:({command,articles})=>{
+        onCommand:({command,articles,number})=>{
             if(command==='newHeadlines'){
               setNewsArticles(articles);
               setActiveArticle(-1)
@@ -22,9 +24,22 @@ const App=()=>{
              else if( command === 'highlight') {
                  setActiveArticle((prevActiveArticle)=> prevActiveArticle+1);
              }
-            }
-        })
-    },[]); 
+             else if (command === 'open') {
+                const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+                const article = articles[parsedNumber - 1];
+      
+                if (parsedNumber > 20) {
+                  alanBtn().playText('Please try that again...');
+                } else if (article) {
+                  window.open(article.url, '_blank');
+                  alanBtn().playText('Opening...');
+                } else {
+                  alanBtn().playText('Please try that again...');
+                }
+              }
+            },
+          });
+        }, [])
 
         return (
             <div>
